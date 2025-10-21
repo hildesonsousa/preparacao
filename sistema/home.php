00611e8e2c36d9510d0151eee965545177bf1464
Hildeson Fonseca Sousa <hildesonsousa@gmail.com>
2025-10-21 10:57:48 -0300

    Remover link de integridade do Bootstrap 4
    Removed integrity attribute from Bootstrap CSS link.

--- sistema/home.php
+++ sistema/home.php
@@ -14,7 +14,7 @@ $nome_usuario = htmlspecialchars($_SESSION['login_nome']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar Produtos</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container text-center">
